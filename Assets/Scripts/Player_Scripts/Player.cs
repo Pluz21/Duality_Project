420b907591c872abd493a4cbd083bb5ef40e9783
Pluz21 <vanleemput.tim@gmail.com>
2024-01-28 21:47:49 +0100

    Working death pop on life 0

--- Assets/Scripts/Player_Scripts/Player.cs
+++ Assets/Scripts/Player_Scripts/Player.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [RequireComponent(typeof(MovementComponent))]
 public class Player : MonoBehaviour
 {
+    public event Action OnLifeTotalReachesZero;
     [SerializeField] int life = 50;
     [SerializeField] int maxLife = 50;
-    MovementComponent movementComponent = null;
+    [SerializeField]MovementComponent movementComponent = null;
     [SerializeField] PlayerAnimation playerAnimation = null;
     [SerializeField] InputComponent inputComponent = null;
+    [SerializeField] UI_DeathMenu deathMenu = null;
 
     public MovementComponent MoveCompoRef => movementComponent;
 
@@ -32,27 +35,46 @@ public class Player : MonoBehaviour
 
     void Init()
     {
+        Time.timeScale = 1.0f;
         movementComponent = GetComponent<MovementComponent>();
         inputComponent = GetComponent<InputComponent>();
         playerAnimation = GetComponent<PlayerAnimation>();
-        movementComponent.OnforwardAxis += playerAnimation.UpdateForwardAnimatorParam;
-        movementComponent.OnRightAxis += playerAnimation.UpdateRightAnimatorParam;
-        movementComponent.OnRotationAxis += playerAnimation.UpdateRotateAnimatorParam;
-        movementComponent.dash += playerAnimation.UpdateIsDashingAnimatorParam;
-        movementComponent.invi += playerAnimation.UpdateIsInvisibleAnimatorParam;
+        InitEvents();
+        life = maxLife;
+       
 
         
     }
 
     void InitEvents()
-    { 
+    {
+        movementComponent.OnforwardAxis += playerAnimation.UpdateForwardAnimatorParam;
+        movementComponent.OnRightAxis += playerAnimation.UpdateRightAnimatorParam;
+        movementComponent.OnRotationAxis += playerAnimation.UpdateRotateAnimatorParam;
+        movementComponent.dash += playerAnimation.UpdateIsDashingAnimatorParam;
+        movementComponent.invi += playerAnimation.UpdateIsInvisibleAnimatorParam;
+        OnLifeTotalReachesZero += DisplayDeathMenu;
         //inputComponent.LeftClick.performed += 
     }
     // Update is called once per frame
     void Update()
     {
         movementComponent.Move();
+        if (life <= 0)
+        {
+        Debug.Log($"life {life}");
+            life = 0;
+            OnLifeTotalReachesZero?.Invoke();
+
+
+        }
+    }
+    void DisplayDeathMenu()
+    {
+        Debug.Log("DeathMenucalled");
+        deathMenu.gameObject.SetActive(true);
+        Time.timeScale = 0;
     }
 
-   
+
 }
--- Assets/Scripts/UI_Scripts/DeathMenu_UI_Scripts/UI_DeathMenu.cs
+++ Assets/Scripts/UI_Scripts/DeathMenu_UI_Scripts/UI_DeathMenu.cs
@@ -2,44 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class UI_DeathMenu : MonoBehaviour
 {
     [SerializeField] GameObject deathMenu = null;
     [SerializeField] ScrollRect kanjiScrollViewRect = null;
     [SerializeField] public float scrollSpeed = 1f;
+    [SerializeField] Button restartButton = null;
+    [SerializeField] Scene sceneToRestart;
+    public GameObject DeathMenu => deathMenu;
     void Start()
     {
         Init();
     }
 
-    void Init()
+    public void Init()
     {
         deathMenu.SetActive(false);
-  
-        
+        restartButton.onClick.AddListener(RestartLevel);
+
+
+
 
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (deathMenu.activeSelf)
+        //if (deathMenu.activeSelf)
+        //{
+        //    //Not working auto scroll
+        //    //float currentVerticalPosition = kanjiScrollViewRect.verticalNormalizedPosition;
+
+        //    //float newVerticalPosition = currentVerticalPosition - Time.deltaTime * scrollSpeed;
+
+        //    //newVerticalPosition = Mathf.Clamp01(newVerticalPosition);
+
+        //    //kanjiScrollViewRect.verticalNormalizedPosition = newVerticalPosition;
+        //}
+
+    }
+    public void RestartLevel()
+    {
+        Debug.Log("restarting");
+        //if (!sceneToRestart.isLoaded)
         {
-            // Get the current vertical position
-            float currentVerticalPosition = kanjiScrollViewRect.verticalNormalizedPosition;
-
-            // Calculate the new vertical position for scrolling
-            float newVerticalPosition = currentVerticalPosition - Time.deltaTime * scrollSpeed;
-
-            // Ensure the value is between 0 and 1
-            newVerticalPosition = Mathf.Clamp01(newVerticalPosition);
-
-            // Set the new vertical position
-            kanjiScrollViewRect.verticalNormalizedPosition = newVerticalPosition;
+            sceneToRestart = SceneManager.GetActiveScene();
         }
+        SceneManager.LoadScene(sceneToRestart.name, LoadSceneMode.Single);
+        deathMenu.SetActive(false);
+        Time.timeScale = 1;
 
     }
 }
