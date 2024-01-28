e7d928204e18f4659376579bd1c7a127e82c7fd8
Pluz21 <vanleemput.tim@gmail.com>
2024-01-28 11:41:00 +0100

    Update to lamp

--- Assets/Scripts/lampOnOff.cs
+++ Assets/Scripts/lampOnOff.cs
@@ -1,17 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class lampOnOff : MonoBehaviour
 {
     [SerializeField] Light light1, light2 = null;
+    [SerializeField] List<Light> allSpotLights = new List<Light>();
+   
     [SerializeField] DayNight refDayNight;
     
     void Start()
     {
+        
+    }
+
+    void Init()
+    {
         refDayNight = FindAnyObjectByType<DayNight>();
         refDayNight.OnNightStarted += LampOn;
         refDayNight.OnDayStarted += LampOff;
+        InitLights(LightType.Spot);
+    }
+
+    void InitLights(LightType _typeToInit)
+    {
+
+        List<Light> _allLights = GetComponents<Light>().ToList();
+        int _size = _allLights.Count;
+        for (int i = 0; i < _size; i++)
+        {
+            if (_allLights[i].type != _typeToInit) return;
+            allSpotLights.Add(_allLights[i]);
+        }
     }
 
     // Update is called once per frame
