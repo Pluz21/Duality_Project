4109f3e0c7dd2a7b10d92873faefb990c071fdea
Pluz21 <vanleemput.tim@gmail.com>
2024-01-28 09:39:50 +0100

    Working intro menu

---
+++ Assets/Scripts/UI_Scripts/IntroMenuScripts/UI_IntroMenuComponent.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI_IntroMenuComponent : MonoBehaviour
+{
+    [SerializeField] Button startGameButton = null;
+    [SerializeField] Button quitGameButton = null;
+    [SerializeField] Scene sceneToStart;
+    [SerializeField] string  sceneToStartString = "TimCityScene";
+    void Start()
+    {
+        Init();
+    }
+    void Init()
+    {
+        startGameButton.onClick.AddListener(StartLevel);
+        quitGameButton.onClick.AddListener(QuitGame);
+    }
+
+    private void StartLevel()
+    {
+       if (!sceneToStart.isLoaded)
+        {
+            SceneManager.GetSceneByName(sceneToStartString);
+        }
+        SceneManager.LoadScene(sceneToStartString, LoadSceneMode.Single);
+
+    }
+    private void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        
+    }
+}
